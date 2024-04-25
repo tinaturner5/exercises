16d64125672c5497d5ba4b904df801a6f1502f6a
tinaturner5 <turner.kristee@gmail.com>
2024-04-25 19:56:11 +0200

    1.,2. exercise done

---
+++ challenge.js
@@ -0,0 +1,36 @@
+/*
+Declare variables called country, continent and population and assign their values according to your own country
+(population in millions).
+
+Log their values into console
+
+Declare a variable called isIsland and set its value according to your country.
+The variable should hold a Boolean value.
+Also declare a variable language, but don't assign it any value yet.
+
+Log the types of isIsland, population, country and language to console
+
+
+Set the value of language to the language spoken where you live
+(some countries have multiple languages, but just choose one).
+
+Think about which variables should be const variables
+(which values will never change, and which might change?).
+Then, change these variables to const.
+
+Try to change one of the changed variables now, and observe what happens
+
+ */
+
+var country = 'Czechia';
+const continent = 'Europe';
+var population = 10;
+var isIsland = false;
+var language = 'czech';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
